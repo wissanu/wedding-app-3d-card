3adbc40d5f1630fa3e93557bc23bd157f3dc1f2a
wissanu (TOR) <thungrut@gmail.com>
2025-03-05 23:30:30 +0700

    remove sky, star and shooting star

--- src/app/starry-sky/starry-sky.component.ts
+++ src/app/starry-sky/starry-sky.component.ts
@@ -38,7 +38,6 @@ export class StarrySkyComponent implements OnInit, OnDestroy {
     this.addMouseMoveListener();
     this.addWindowResizeListener();
     this.addClickListener(); // Add click event listener
-    this.createShootingStar();
   }
 
   ngOnDestroy(): void {
@@ -57,9 +56,7 @@ export class StarrySkyComponent implements OnInit, OnDestroy {
       canvas,
       antialias: true,
     });
-    this.renderer.setClearColor(new THREE.Color('#1D2951'));
-    //rgb(133, 130, 100)
-    // #1c1624
+    
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.setRendererSize();
     
@@ -86,13 +83,6 @@ export class StarrySkyComponent implements OnInit, OnDestroy {
     const isMobile = window.innerWidth <= 768; // Check if the device is mobile
     this.camera.position.z = isMobile ? 2.5 : 1.5; // Zoom out for mobile devices
 
-    // Geometries
-    const geometrys = [new THREE.BufferGeometry(), new THREE.BufferGeometry(), new THREE.BufferGeometry(), new THREE.BufferGeometry()];
-    geometrys[0].setAttribute('position', new THREE.BufferAttribute(this.getRandomParticlePos(7500), 3));
-    geometrys[1].setAttribute('position', new THREE.BufferAttribute(this.getRandomParticlePos(15000), 3));
-    geometrys[2].setAttribute('position', new THREE.BufferAttribute(this.getRandomParticlePos(2500), 3));
-    geometrys[3].setAttribute('position', new THREE.BufferAttribute(this.getRandomParticlePos(3000), 3));
-
     // Texture Loader with Loading Manager
     const loadingManager = new THREE.LoadingManager(
       () => {
@@ -116,49 +106,6 @@ export class StarrySkyComponent implements OnInit, OnDestroy {
     // Flip the back texture
     backTexture.repeat.x = -1;
     backTexture.offset.x = 1;
-
-    // Materials for particles (sp1 and sp2)
-    const particleMaterials = [
-      new THREE.PointsMaterial({
-        size: 0.005,
-        map: loader.load('assets/sp1.png'),
-        transparent: true,
-        depthWrite: false, // Disable depth writing for particles
-      }),
-      new THREE.PointsMaterial({
-        size: 0.0075,
-        map: loader.load('assets/sp2.png'),
-        transparent: true,
-        depthWrite: false, // Disable depth writing for particles
-      }),new THREE.PointsMaterial({
-        size: 0.075,
-        map: loader.load('assets/sp2.png'),
-        transparent: true,
-        depthWrite: false, // Disable depth writing for particles
-      }),new THREE.PointsMaterial({
-        size: 0.005,
-        map: loader.load('assets/sp1.png'),
-        transparent: true,
-        depthWrite: false, // Disable depth writing for particles
-      }),
-    ];
-
-    // Points (starry sky particles)
-    this.starsT1 = new THREE.Points(geometrys[0], particleMaterials[0]);
-    this.starsT2 = new THREE.Points(geometrys[1], particleMaterials[1]);
-    this.starsT3 = new THREE.Points(geometrys[2], particleMaterials[2]);
-    this.starsT4 = new THREE.Points(geometrys[3], particleMaterials[3]);
-
-    // Set renderOrder for particles (render first)
-    this.starsT1.renderOrder = 1;
-    this.starsT2.renderOrder = 1;
-    this.starsT3.renderOrder = 1;
-    this.starsT4.renderOrder = 1;
-
-    this.scene.add(this.starsT1);
-    this.scene.add(this.starsT2);
-    this.scene.add(this.starsT3);
-    this.scene.add(this.starsT4);
 
     // Add 3D Card
     this.add3DCard(frontTexture, backTexture);
@@ -203,13 +150,6 @@ export class StarrySkyComponent implements OnInit, OnDestroy {
     this.scene.add(this.cardGroup);
   }
 
-  private getRandomParticlePos(particleCount: number): Float32Array {
-    const arr = new Float32Array(particleCount * 3);
-    for (let i = 0; i < particleCount; i++) {
-      arr[i] = (Math.random() - 0.5) * 10;
-    }
-    return arr;
-  }
 
   private addWindowResizeListener(): void {
     window.addEventListener('resize', this.onWindowResize.bind(this));
@@ -250,100 +190,6 @@ export class StarrySkyComponent implements OnInit, OnDestroy {
     this.shouldRotate = !this.shouldRotate; // Toggle the rotation flag
   }
 
-
-  private createShootingStar(): void {
-    const starGeometry = new THREE.BufferGeometry();
-    const numParticles = 1;
-    const positions = new Float32Array(numParticles * 3);
-    positions[0] = 0; // X
-    positions[1] = 0; // Y
-    positions[2] = 0; // Z
-    starGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
-
-    const starMaterial = new THREE.PointsMaterial({
-      size: 0.25,
-      color: 0xEEE8AA,
-      map: this.starTexture, // Use circular texture
-      transparent: true,
-      opacity: 1.0,
-      depthWrite: false,
-      blending: THREE.AdditiveBlending
-    });
-
-    this.shootingStar = new THREE.Points(starGeometry, starMaterial);
-    this.scene.add(this.shootingStar);
-    this.moveShootingStar();
-  }
-
-  private moveShootingStar(): void {
-    const startX = Math.random() * 4 - 2;
-    const startY = Math.random() * 2 + 1;
-    const endX = startX - 2;
-    const endY = startY - 1;
-    
-    this.shootingStar.position.set(startX, startY, -1);
-    this.createTrail(startX, startY);
-    
-    gsap.to(this.shootingStar.position, {
-      x: endX, y: endY, duration: this.starSpeed, ease: 'power1.out',
-      onUpdate: () => this.createTrail(this.shootingStar.position.x, this.shootingStar.position.y),
-      onComplete: () => {
-        gsap.to(this.shootingStar.material, {
-          opacity: 0,
-          duration: this.trailDecaySpeed,
-          onComplete: () => {
-            this.scene.remove(this.shootingStar);
-            this.createShootingStar();
-          }
-        });
-      }
-    });
-  }
-
-  private createTrail(x: number, y: number): void {
-    const trailGeometry = new THREE.BufferGeometry();
-    const numParticles = 20;
-    const positions = new Float32Array(numParticles * 3);
-    const colors = new Float32Array(numParticles * 3);
-
-    for (let i = 0; i < numParticles; i++) {
-      positions[i * 3] = x - (Math.random() * 0.05 - 0.025); // Reduce trail width by half
-      positions[i * 3 + 1] = y - (Math.random() * 0.05 - 0.025);
-      positions[i * 3 + 2] = -1;
-      
-      const color = Math.random() < 0.4 ? new THREE.Color(0xf0d08f) : new THREE.Color(0xffd700);
-      colors[i * 3] = color.r;
-      colors[i * 3 + 1] = color.g;
-      colors[i * 3 + 2] = color.b;
-    }
-
-    trailGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
-    trailGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
-    
-    const trailMaterial = new THREE.PointsMaterial({
-      // color: 0xffd700,
-      size: 0.005, // Use defined size
-      transparent: true,
-      opacity: 1,
-      map: this.trailTexture,
-      blending: THREE.AdditiveBlending,
-      depthWrite: false,
-      vertexColors: true
-    });
-    
-    const trail = new THREE.Points(trailGeometry, trailMaterial);
-    this.scene.add(trail);
-    this.trails.push(trail);
-    
-    gsap.to(trail.material, {
-      opacity: 0,
-      duration: this.trailDecaySpeed * (Math.random() * 2 + 1),
-      onComplete: () => {
-        this.scene.remove(trail);
-        this.trails = this.trails.filter(t => t !== trail);
-      }
-    });
-  }
   private animate(): void {
     if (!this.texturesLoaded) return;
   
@@ -352,23 +198,7 @@ export class StarrySkyComponent implements OnInit, OnDestroy {
       if (this.cardGroup && this.shouldRotate) {
         this.cardGroup.rotation.y += 0.01;
       }
-  
-      // Flash effect for particles
-      const flashSpeed = 0.01; // Speed of the flash effect
-      const scale = Math.sin(time * flashSpeed) * 0.5 + 1; // Oscillate between 0.5 and 1.5
-      const dimmer = 0.001; // Speed of the flash effect
-      const opacity = Math.sin(time * dimmer) * 0.5 + 0.5; // Oscillate between 0 and 1
-  
-      // Apply the flash effect to sp1 and sp2 particles
-      if (this.starsT1.material instanceof THREE.PointsMaterial) {
-        this.starsT1.material.size = 0.005 * scale; // Adjust the base size as needed
-        this.starsT1.material.opacity = opacity;
-      }
-      if (this.starsT2.material instanceof THREE.PointsMaterial) {
-        this.starsT2.material.size = 0.0075 * scale; // Adjust the base size as needed
-      }
 
-      this.starsT1.geometry.attributes['position'].needsUpdate = true;
       this.renderer.render(this.scene, this.camera);
       requestAnimationFrame(render);
     };
